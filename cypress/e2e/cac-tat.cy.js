a7309fda1ebb9dc0ad0b355e3c27df9fe5308218
AlvaroBiazonPessoa <alvarobiazons@gmail.com>
2025-01-09 16:11:12 -0300

    feat: Adicionando arquivos do cypress

---
+++ cypress/e2e/cac-tat.cy.js
@@ -0,0 +1,7 @@
+describe('Central de Atendimento ao Cliente TAT', () => {
+
+  it('verificar o título da aplicação', () => {
+    cy.visit('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
+  })
+
+})
