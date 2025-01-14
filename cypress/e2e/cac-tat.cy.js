34bcfbb90d3a7eb1d6d51d0a023e0f68388fbe09
AlvaroBiazonPessoa <alvarobiazons@gmail.com>
2025-01-13 22:10:10 -0300

    feat: Adicionando caso de teste para verificar o título da aplicação

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -1,7 +1,9 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
 
   it('verificar o título da aplicação', () => {
+    const CatTatTitle = 'Central de Atendimento ao Cliente TAT'
     cy.visit('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
+    cy.title().should('be.equal', CatTatTitle)
   })
 
 })
