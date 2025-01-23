bdd9cd4a4c85dd0fd6ff4029ee834457cb6a00c9
AlvaroBiazonPessoa <alvarobiazons@gmail.com>
2025-01-23 20:54:39 -0300

    feat: Adicionando bloco beforeEach() para acessar site toda vez a cada caso de teste

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -1,8 +1,12 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
 
+  beforeEach(() => {
+    const cacTatUrl = 'https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html'
+    cy.visit(cacTatUrl)
+  })
+
   it('verificar o título da aplicação', () => {
     const catTatTitle = 'Central de Atendimento ao Cliente TAT'
-    cy.visit('https://cac-tat-v3.s3.eu-central-1.amazonaws.com/index.html')
     cy.title().should('be.equal', catTatTitle)
   })
 
