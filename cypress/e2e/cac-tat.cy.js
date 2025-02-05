2d5df99954925cac01996f32a5ad245993a49bd4
AlvaroBiazonPessoa <alvarobiazons@gmail.com>
2025-02-04 21:19:37 -0300

    feat: Adicionando caso de teste que vai exibir mensagem de erro ao passar o valor do campo e-mail com a formatação errada

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -37,4 +37,31 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.get(successMessageBoxElementClass).should('be.visible')
   })
 
+  it('exibir mensagem de erro ao submeter o formulário com um e-mail com formatação inválida', () => {
+    const valueOfTheMandatoryFieldNome = 'Mariana'
+    const valueOfTheMandatoryFieldSobrenome = 'Bridi Vital'
+    const incorrectValueOfTheMandatoryFieldEmail = 'mariana.b.vital#email.com.@.br'
+    const valueOfTheMandatoryFieldFeedback = 'Adorei!'
+    const selectorOfTheFieldNome = '#firstName'
+    const selectorOfTheFieldSobrenome = '#lastName'
+    const selectorOfTheFieldEmail = '#email'
+    const selectorOfTheFieldFeedback = '#open-text-area'
+    const selectorOfTheButtonEnviar = '#white-background > form > button'
+    const errorMessageBoxElementClass = '.error'
+    cy.get(selectorOfTheFieldNome).should('be.visible')
+    cy.get(selectorOfTheFieldNome).type(valueOfTheMandatoryFieldNome)
+    cy.get(selectorOfTheFieldNome).should('have.value', valueOfTheMandatoryFieldNome)
+    cy.get(selectorOfTheFieldSobrenome).should('be.visible')
+    cy.get(selectorOfTheFieldSobrenome).type(valueOfTheMandatoryFieldSobrenome)
+    cy.get(selectorOfTheFieldSobrenome).should('have.value', valueOfTheMandatoryFieldSobrenome)
+    cy.get(selectorOfTheFieldEmail).should('be.visible')
+    cy.get(selectorOfTheFieldEmail).type(incorrectValueOfTheMandatoryFieldEmail)
+    cy.get(selectorOfTheFieldEmail).should('have.value', incorrectValueOfTheMandatoryFieldEmail)
+    cy.get(selectorOfTheFieldFeedback).should('be.visible')
+    cy.get(selectorOfTheFieldFeedback).type(valueOfTheMandatoryFieldFeedback)
+    cy.get(selectorOfTheFieldFeedback).should('have.value', valueOfTheMandatoryFieldFeedback)
+    cy.get(selectorOfTheButtonEnviar).click()
+    cy.get(errorMessageBoxElementClass).should('be.visible')
+  })
+
 })
