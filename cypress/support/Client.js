06c5bd3893fa5aa90d7265aaf62d9f3d9a05e022
AlvaroBiazonPessoa <alvarobiazons@gmail.com>
2025-02-10 22:50:26 -0300

    feat: Adicionando classe Client

---
+++ cypress/support/Client.js
@@ -0,0 +1,11 @@
+class Client {
+
+    constructor(firstName, lastName, email, telephone, feedback) {
+        this.firstName = firstName
+        this.lastName = lastName
+        this.email
+        this.telephone
+        this.feedback = feedback
+    }
+
+}
