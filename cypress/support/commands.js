e9c48cbe0d466da6788d0618b31dc14cbc26a303
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-02-17 20:36:56 -0300

    feat: Adicionando comando customizado gui_fillMandatoryFieldsAndSubmit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,16 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('gui_fillMandatoryFieldsAndSubmit', (selectorOfTheFieldName, selectorOfTheFieldLastName, selectorOfTheFieldEmail, selectorOfTheFieldFeedback, selectorOfTheButtonSend, successMessageBoxElementClass, client) => {
+    cy.get(selectorOfTheFieldName).should('be.visible')
+    cy.get(selectorOfTheFieldName).type(client.firstName)
+    cy.get(selectorOfTheFieldName).should('have.value', client.firstName)
+    cy.get(selectorOfTheFieldLastName).should('be.visible')
+    cy.get(selectorOfTheFieldLastName).type(client.lastName)
+    cy.get(selectorOfTheFieldLastName).should('have.value', client.lastName)
+    cy.get(selectorOfTheFieldEmail).should('be.visible')
+    cy.get(selectorOfTheFieldEmail).type(client.email)
+    cy.get(selectorOfTheFieldEmail).should('have.value', client.email)
+    cy.get(selectorOfTheFieldFeedback).should('be.visible')
+    cy.get(selectorOfTheFieldFeedback).type(client.feedback)
+    cy.get(selectorOfTheFieldFeedback).should('have.value', client.feedback)
+    cy.get(selectorOfTheButtonSend).click()
+    cy.get(successMessageBoxElementClass).should('be.visible')
+})
