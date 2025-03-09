02235a1a3ef9a1bcbf57e07fce82c62158b1f727
AlvaroBiazonPessoa <alvarobiazon@hotmail.com>
2025-03-09 12:17:59 -0300

    fix: Alterando caminho da importação do arquivo Clinet.js

--- cypress/e2e/cac-tat.cy.js
+++ cypress/e2e/cac-tat.cy.js
@@ -1,5 +1,5 @@
 const { faker } = require('@faker-js/faker')
-const Client = require('../support/Client')
+const Client = require('../fixtures/Client')
 
 describe('Central de Atendimento ao Cliente TAT', () => {
 
